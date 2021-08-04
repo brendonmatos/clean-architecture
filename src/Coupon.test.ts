95186edfb9751b4f4620a50366f1a50298c582ec
br <brendonferreiradm@gmail.com>
2021-08-03 21:32:47 -0300

    coupon expire working

---
+++ src/Coupon.test.ts
@@ -0,0 +1,15 @@
+import Coupon from "./Coupon"
+
+test("Should refuse expired coupons", function() {
+
+    const coupon = new Coupon({code: "12093123", discount: 10, expireDate: new Date('2020-02-01') })
+    expect(coupon.isExpired()).toBe(true)
+
+})
+
+test("Should accept valida coupons", function() {
+
+    const coupon = new Coupon({code: "12093123", discount: 10, expireDate: new Date('2021-11-10') })
+    expect(coupon.isExpired()).toBe(false)
+
+})
--- src/Coupon.ts
+++ src/Coupon.ts
@@ -1,17 +1,26 @@
  
-interface ICoupon { code: string; discount: number }
+interface ICoupon { code: string; discount: number, expireDate: Date }
 
 export default class Coupon {
     code: string;
     discount: number;
+    expireDate: Date;
 
     constructor (coupon: ICoupon) {
         this.code = coupon.code;
         this.discount = coupon.discount;
+        this.expireDate = coupon.expireDate;
     }
 
     apply (price: number): number {
         const discount = (price * this.discount) / 100;
         return price - discount;
+    }
+
+
+    isExpired(): boolean {
+        console.log(Date.now(), this.expireDate.getTime())
+
+        return Date.now() > this.expireDate.getTime();
     }
 }
