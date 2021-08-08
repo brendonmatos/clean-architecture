a68dc81240f33e232d187c262a594cbd144f87ae
br <brendonferreiradm@gmail.com>
2021-08-08 20:28:15 -0300

    more tests to orders repository

--- src/OrdersRepository.test.ts
+++ src/OrdersRepository.test.ts
@@ -17,3 +17,18 @@ test("should be create order entry with 3 items and discount", async function() 
     expect(match?.length).toBe(1)
     expect(match?.[0]).toBe(date.getFullYear().toString())
 })
+
+test("should be able to create and restore order", async function() {
+    const orderRepository = new OrdersRepository();
+    const toCreate: IClient = {
+        cpf: "186.360.540-10"
+    } 
+    const date = new Date()
+    const order = new Order(toCreate, date)
+    await orderRepository.save(order)
+    expect(order.id).toBeDefined()
+    const returnedOrder = await orderRepository.getById(order.id)
+    expect(returnedOrder?.id).toBeDefined()
+    expect(returnedOrder?.id).toBe(order.id)
+
+})
